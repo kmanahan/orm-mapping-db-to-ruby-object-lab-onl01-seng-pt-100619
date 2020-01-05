5717ddb6d7f0860ac7f3ff969fba784c6ff74e4f
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-01-05 14:01:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -67,7 +67,7 @@ class Student
   end
   
   def self.first_X_students_in_grade_10(x)
-    sql = "SELECT * FROM students WHERE grade = 10"
+    sql = "SELECT * FROM students WHERE grade = 10 LIMIT ?"
     DB[:conn].execute(sql, x)
     end
 end
